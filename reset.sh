bc525f115078e2c3bb0d74c6a312f039f9376b5a
jake2323livecom <jake.2323@live.com>
2022-03-12 14:46:53 -0500

    removing static files,

--- reset.sh
+++ reset.sh
@@ -3,6 +3,3 @@ docker rmi jcu_ball_web
 docker rmi jcu_ball_nginx
 docker-compose up -d --build
 sleep 1
-docker-compose exec web python manage.py makemigrations
-docker-compose exec web python manage.py migrate
-docker-compose exec web python manage.py collectstatic
