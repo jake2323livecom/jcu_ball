d10906e12bcc6d07aa1ef59940355c08fe3e3a62
jake2323livecom <jake.2323@live.com>
2022-03-12 00:54:49 +0000

    replacing image name with name of current dir

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 docker-compose down -v
-docker rmi jcu_ball_web
-docker rmi jcu_ball_nginx
+docker rmi $(pwd)_web
+docker rmi $(pwd)_nginx
 docker-compose up -d --build
 sleep 1
 docker-compose exec web python manage.py makemigrations
